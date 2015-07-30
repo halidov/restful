eb77c9dd1fa8bea68a39e83086cc74b1e5eaf0e1
Zelimkhan Khalidov <ondalom@gmail.com>
2015-07-30 17:49:45 +0300

    fix

--- app/Http/Middleware/MyAuthenticateWithBasicAuth.php
+++ app/Http/Middleware/MyAuthenticateWithBasicAuth.php
@@ -5,33 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
 
-class MyAuthenticateWithBasicAuth
+class MyAuthenticateWithBasicAuth extends \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth
 {
-    /**
-     * The guard instance.
-     *
-     * @var \Illuminate\Contracts\Auth\Guard
-     */
-    protected $auth;
-
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Guard  $auth
-     * @return void
-     */
-    public function __construct(Guard $auth)
-    {
-        $this->auth = $auth;
-    }
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
         return $this->auth->basic('login') ?: $next($request);
